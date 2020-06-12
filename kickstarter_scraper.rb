7234e229e34a0a22e7a71050298540f1cf3f20a7
isabella g henriques <isabellagh88@gmail.com>
2020-06-12 02:36:35 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,6 +13,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   binding.pry
   kickstarter.css("li.project.grid_4").first
+  project.css("h2.bbcard_name strong a").text
 end
  
 create_project_hash
