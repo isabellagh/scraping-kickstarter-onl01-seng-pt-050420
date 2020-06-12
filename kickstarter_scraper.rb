28e5aabdb607fce435ef4297818a808c4ca58811
isabella g henriques <isabellagh88@gmail.com>
2020-06-12 02:50:31 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,9 +17,7 @@ def create_project_hash
   projects = {}
   
    # Iterate through the projects
-   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
-   end
+  kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
@@ -29,7 +27,6 @@ def create_project_hash
     }
   kickstarter.css("li.project.grid_4").first
   project.css("h2.bbcard_name strong a").text
-  projects
 end
  
 create_project_hash
